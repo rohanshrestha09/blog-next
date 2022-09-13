563e614997ecfaff53ee8a8304d9eb160daedaa3
rohanshrestha09 <shresthacr11@gmail.com>
2022-09-13 20:50:20 +0545

    Multiline truncate

--- components/Blogs/BlogList.tsx
+++ components/Blogs/BlogList.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image';
 import { Avatar, Button, Divider, Space, Tag } from 'antd';
 import moment from 'moment';
 import he from 'he';
-import { BsDot, BsHeart } from 'react-icons/bs';
+import { BsHeart } from 'react-icons/bs';
 import { VscComment } from 'react-icons/vsc';
-import { FaEdit } from 'react-icons/fa';
 import { FiEdit3 } from 'react-icons/fi';
+import { MdOutlinePublishedWithChanges, MdOutlineUnpublished } from 'react-icons/md';
 
 interface Props {
   editable?: boolean;
@@ -33,20 +33,10 @@ const BlogList: React.FC<Props> = ({
       <div className='w-full flex flex-col gap-3 sm:px-10 py-4'>
         <Space>
           {editable ? (
-            <>
-              <Button className='btn min-h-fit h-fit px-2 gap-1 focus:bg-[#021027] capitalize leading-none rounded-full text-xs'>
-                Edit
-                <FiEdit3 />
-              </Button>
-
-              <Tag
-                className={`${
-                  isPublished ? 'bg-[#36D399]' : 'bg-[#FF5500]'
-                } text-gray-100 border-0 rounded-full mx-0`}
-              >
-                {isPublished ? 'Published' : 'Unpublished'}
-              </Tag>
-            </>
+            <Button className='btn min-h-fit h-fit px-2 gap-1 focus:bg-[#021027] capitalize leading-none rounded-full text-xs'>
+              Edit
+              <FiEdit3 />
+            </Button>
           ) : (
             <>
               {authorImage ? (
@@ -62,17 +52,21 @@ const BlogList: React.FC<Props> = ({
               <p className='break-all'>{authorName}</p>
             </>
           )}
-          <BsDot className='text-gray-500' />
+          <span className='text-2xl leading-none tracking-tighter text-gray-400'>&#x22C5;</span>
           <p className='text-gray-500 text-xs'>{moment(createdAt).format('ll').slice(0, -6)}</p>
         </Space>
 
-        <div className='w-full flex justify-between sm:gap-12 gap-6 break-all'>
+        <div className='w-full flex justify-between sm:gap-12 gap-6 break-words'>
           <Space direction='vertical' size={4}>
-            <p className='sm:text-xl text-base font-bold leading-none'>{title}</p>
-            <p className=''>{he.decode(content.replace(/<[^>]+>/g, ''))}</p>
+            <p className='sm:text-xl text-base font-bold sm:leading-none leading-snug multiline-truncate-title'>
+              {title}
+            </p>
+            <p className='leading-loose multiline-truncate-content'>
+              {he.decode(content.replace(/<[^>]+>/g, ''))}
+            </p>
           </Space>
 
-          <span className='relative min-w-[7.5rem] min-h-[7.5rem] max-h-[7.5rem]'>
+          <span className='relative min-w-[4rem] min-h-[4rem] sm:min-w-[7.5rem] sm:min-h-[7.5rem] sm:max-h-[7.5rem]'>
             <Image alt='' src={image} height={120} width={120} layout='fill' />
           </span>
         </div>
@@ -99,6 +93,27 @@ const BlogList: React.FC<Props> = ({
               <VscComment />
               {1}
             </Space>
+
+            {editable && (
+              <>
+                <Divider className='bg-slate-200' type='vertical' />
+                {isPublished ? (
+                  <span
+                    className='flex items-center tooltip sm:tooltip-bottom tooltip-left'
+                    data-tip={isPublished ? 'Published' : 'Unpublished'}
+                  >
+                    <MdOutlinePublishedWithChanges />
+                  </span>
+                ) : (
+                  <span
+                    className='flex items-center tooltip sm:tooltip-bottom tooltip-left'
+                    data-tip={isPublished ? 'Published' : 'Unpublished'}
+                  >
+                    <MdOutlineUnpublished />
+                  </span>
+                )}
+              </>
+            )}
           </Space>
         </div>
       </div>
