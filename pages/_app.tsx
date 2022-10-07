6e08aa52b0111410016e2f1a5024cdb9f0139f56
rohanshrestha09 <shresthacr11@gmail.com>
2022-10-07 18:58:56 +0545

    Added Users skeleton and follow api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import 'antd/dist/antd.dark.min.css';
+import '../styles/antd.dark.min.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { useState } from 'react';
