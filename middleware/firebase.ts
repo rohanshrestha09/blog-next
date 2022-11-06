95bc7e311bd77e3b257d9ce2baac4c7003f85dfc
rohanshrestha09 <shresthacr11@gmail.com>
2022-11-06 19:42:13 +0545

    Edit env files

--- middleware/firebase.ts
+++ middleware/firebase.ts
@@ -1,10 +1,9 @@
-import firebaseAdmin from 'firebase-admin';
-
-const serviceAccount = require('../blog-sansar-firebase-adminsdk-8snwe-96b9089a8c.json');
+import firebaseAdmin, { ServiceAccount } from 'firebase-admin';
+import serviceAccount from '../blog-sansar-firebase-adminsdk-8snwe-96b9089a8c';
 
 try {
   firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert(serviceAccount),
+    credential: firebaseAdmin.credential.cert(serviceAccount as ServiceAccount),
     storageBucket: 'gs://blog-sansar.appspot.com',
   });
 } catch (error: Error | any) {
