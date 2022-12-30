95b841815ad60f9b6e8c1dd5a82dac42e215dbfd
Rohan Shrestha <87956956+rohanshrestha09@users.noreply.github.com>
2022-12-30 20:42:16 +0545

    Delete dist directory

--- dist/controller/blog/genre.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.genre = void 0;
-const server_interface_1 = require("../../server.interface");
-const asyncHandler = require('express-async-handler');
-exports.genre = asyncHandler(async (req, res) => {
-    res.setHeader('Cache-Control', 'public,max-age=86400');
-    return res.status(200).json({ data: server_interface_1.genre, message: 'Genre Fetched Successfully' });
-});
