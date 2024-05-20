64cbd052386ce9c4d84de97d567d50cd07ea8421
Rohan Shrestha <rohanshrestha.dev@gmail.com>
2024-05-20 11:28:32 +0545

    allow cors for blogsansar.me

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ const nextConfig = {
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://blogsansar.me' },
           { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
           {
             key: 'Access-Control-Allow-Headers',
